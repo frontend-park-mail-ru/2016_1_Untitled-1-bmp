a45d1d96b405f2dd99322cee9553a506bbd12fa2
Nikita <nks.off@gmail.com>
2016-02-20 14:31:32 +0300

    <init> add materialize sass, fonts + grunt tasks for it

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,38 @@
 module.exports = function (grunt) {
 
   grunt.initConfig({
+    sass: {
+      // https://www.npmjs.com/package/grunt-sass
+      dev: {
+        options: {
+          outputStyle: 'expanded',
+          sourcemap: false
+        },
+        files: {
+          'dist/css/main.css': 'assets/sass/main.scss'
+        }
+      },
+      prod: {
+        options: {
+          outputStyle: 'compressed',
+          sourcemap: false
+        },
+        files: {
+          'dist/css/main.css': 'assets/sass/main.scss'
+        }
+      }
+    },
+
+    copy: {
+      // https://www.npmjs.com/package/grunt-contrib-copy
+      fonts: {
+        cwd: 'assets/fonts',
+        src: [ '**' ],
+        dest: 'dist/fonts',
+        expand: true
+      }
+    },
+
     shell: {
       // https://www.npmjs.com/package/grunt-shell
       options: {
@@ -19,7 +51,7 @@ module.exports = function (grunt) {
           expand: true,
           cwd: 'templates',
           src: '*.xml',
-          dest: 'public_html/js/tmpl'
+          dest: 'dist/js/tmpl'
         }],
         options: {
           template: function (data) {
@@ -41,13 +73,29 @@ module.exports = function (grunt) {
           interrupt: true,
           atBegin: true
         }
+      },
+      sass: {
+        files: ['assets/sass/**/*'],
+        tasks: ['sass:dev'],
+        options: {
+          interrupt: true,
+          atBegin: true
+        }
+      },
+      copy: {
+        files: ['assets/fonts/**/*'],
+        tasks: ['copy:fonts'],
+        options: {
+          interrupt: true,
+          atBegin: true
+        }
       }
     },
 
     concurrent: {
       // https://www.npmjs.com/package/grunt-concurrent
       dev: {
-        tasks: ['shell', 'watch']
+        tasks: ['shell:dev', 'watch']
       },
       options: {
         logConcurrentOutput: true
@@ -55,11 +103,14 @@ module.exports = function (grunt) {
     },
   });
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-concurrent');
-  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-fest');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-shell');
 
   grunt.registerTask('dev', ['concurrent:dev']);
-
+  grunt.registerTask('prod', ['fest', 'sass:prod', 'copy:fonts']);
+  grunt.registerTask('default', ['dev']);
 };
