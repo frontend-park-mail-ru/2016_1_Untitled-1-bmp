72a3672259973beb3a12a4b7d9364bd151de900e
Nikita <nks.off@gmail.com>
2016-02-29 18:16:41 +0300

    add collection to scoreboard

--- assets/js/views/scoreboard.js
+++ assets/js/views/scoreboard.js
@@ -6,10 +6,27 @@ define(function(require) {
 
   var scoreboardView = Backbone.View.extend({
     template: scoreboardTemplate,
+    collection: null,
     initialize: function() {
+      var Collection = Backbone.Collection.extend({
+        model: function(attrs, options) {
+          return new scoreModel(attrs, options);
+        }
+      });
+
+      this.collection = new Collection();
+
+      this.collection.add([
+        {
+          name: 'AAA',
+          score: 100
+        },
+        {
+          name: 'New name'
+        }
+      ]);
+
       this.render();
-      var a = new scoreModel({name: 'aaa', score: 100});
-      console.log(a);
     },
 
     render: function() {
