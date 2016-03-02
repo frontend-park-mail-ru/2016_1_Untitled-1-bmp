c2863f874f5e3589457d9993a9dca4d7bef3265b
Nikita <nks.off@gmail.com>
2016-03-02 19:01:08 +0300

    add user model and tests for it

---
+++ assets/js/models/user.js
@@ -0,0 +1,48 @@
+define(function(require) {
+  var $ = require('jquery');
+  var Backbone = require('backbone');
+
+  var User = Backbone.Model.extend({
+    // login, name, email
+    initialize: function() {
+    },
+    validate: function(attrs, options) {
+      var errors = [];
+
+      if($.trim(attrs.login) === '') {
+        errors.push({
+          field: 'login',
+          error: 'Вы не указали логин'
+        });
+      }
+
+      if($.trim(attrs.name) === '') {
+        errors.push({
+          field: 'name',
+          error: 'Вы не указали ваше имя'
+        });
+      }
+
+      var emailRegexp = /^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@([a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/;
+
+      if($.trim(attrs.email) === '') {
+        errors.push({
+          field: 'email',
+          error: 'Вы не указали email'
+        });
+      }
+      else if(!emailRegexp.test($.trim(attrs.email))) {
+        errors.push({
+          field: 'email',
+          error: 'Вы указали некорректный email'
+        });
+      }
+
+      if(errors.length > 0) {
+        return errors;
+      }
+    }
+  });
+
+  return User;
+});
--- assets/js/tests.js
+++ assets/js/tests.js
@@ -1,6 +1,7 @@
 define(function(require) {
   var tests = [
-    'models/score.test'
+    'models/score.test',
+    'models/user.test'
   ];
   require(tests, function() {
   });
