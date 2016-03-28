550cc2126bfa67c6bd18eed947b433b61fdf88d7
Nikita <nks.off@gmail.com>
2016-03-28 23:24:56 +0300

    remove name from user model

--- assets/js/models/user.js
+++ assets/js/models/user.js
@@ -3,35 +3,36 @@ define(function(require) {
   var Backbone = require('backbone');
 
   var User = Backbone.Model.extend({
-    // login, name, email
+    // login, email
     initialize: function() {
     },
     validate: function(attrs, options) {
       var errors = [];
 
-      if($.trim(attrs.login) === '') {
+      _.each(['login', 'email'], function(v) {
+        if(attrs[v] === undefined) {
+          attrs[v] =  '';
+        }
+        attrs[v] = $.trim(attrs[v]);
+      });
+
+
+      if(attrs.login === '') {
         errors.push({
           field: 'login',
           error: 'Вы не указали логин'
         });
       }
 
-      if($.trim(attrs.name) === '') {
-        errors.push({
-          field: 'name',
-          error: 'Вы не указали ваше имя'
-        });
-      }
-
       var emailRegexp = /^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@([a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/;
 
-      if($.trim(attrs.email) === '') {
+      if(attrs.email === '') {
         errors.push({
           field: 'email',
           error: 'Вы не указали email'
         });
       }
-      else if(!emailRegexp.test($.trim(attrs.email))) {
+      else if(!emailRegexp.test(attrs.email)) {
         errors.push({
           field: 'email',
           error: 'Вы указали некорректный email'
--- assets/js/models/user.test.js
+++ assets/js/models/user.test.js
@@ -4,20 +4,19 @@ define(function(require) {
   QUnit.module('models/user');
 
   QUnit.test('Создание объекта User', function() {
-    var u = new User({ login: 'le-me', name: 'Mr.White', email: 'ya@ya.ru' });
+    var u = new User({ login: 'le-me', email: 'ya@ya.ru' });
 
     QUnit.ok(u instanceof Backbone.Model, 'User со всеми заполненными полями');
   });
 
   QUnit.test('Валидация User', function() {
     var toTest = [
-      [{}, 'Все поля пустые', ['login', 'name', 'email']],
+      [{}, 'Все поля пустые', ['login', 'email']],
       [{ login: '' }, 'Пустой login', ['login']],
       [{ login: '      ' }, 'login из пробелов', ['login']],
-      [{ name: '' }, 'Пустой name', ['name']],
       [{ email: '' }, 'Пустой email', ['email']],
       [{ email: 'le-meyandex.ru' }, 'Email без собачки', ['email']],
-      [{ login: 'my-login', name: 'My name', email: 'le-me@yandex.ru' }, 'Валидный кейс', []]
+      [{ login: 'my-login', email: 'le-me@yandex.ru' }, 'Валидный кейс', []]
     ];
 
     _.each(toTest, function(item) {
