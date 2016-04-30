ac29ca4ebc31a8a8b7e1e84e70168295377c0d11
Nikita <nks.off@gmail.com>
2016-04-30 11:42:37 +0300

    clear project

--- assets/js/router.js
+++ assets/js/router.js
@@ -3,34 +3,21 @@ define(function (require) {
   var _ = require('underscore');
   var app = require('app');
   var MainView = require('views/main'),
-      ScoreboardView = require('views/scoreboard'),
-      GameView = require('views/game'),
-      LoginView = require('views/login'),
-      SignupView = require('views/signup');
+      ScoreboardView = require('views/scoreboard');
 
   var viewManager = require('views/manager');
 
   var mainView = new MainView();
   var scoreboardView = new ScoreboardView();
-  var gameView = new GameView();
-  var loginView = new LoginView();
-  var signupView = new SignupView();
 
   _.each([mainView,
-         scoreboardView,
-         gameView,
-         loginView,
-         signupView], function(view) {
+         scoreboardView], function(view) {
            viewManager.addView(view);
   });
 
   var Router = Backbone.Router.extend({
     routes: {
       'scoreboard': 'scoreboardAction',
-      'game':       'gameAction',
-      'login':      'loginAction',
-      'logout':     'logoutAction',
-      'signup':     'signupAction',
       '*default':   'defaultAction',
     },
 
@@ -44,23 +31,6 @@ define(function (require) {
 
     scoreboardAction: function () {
       scoreboardView.show();
-    },
-
-    gameAction: function () {
-      gameView.show();
-    },
-
-    loginAction: function () {
-      loginView.show();
-    },
-
-    logoutAction: function () {
-      app.getSession().logout();
-      this.go('');
-    },
-
-    signupAction: function () {
-      signupView.show();
     }
   });
 
