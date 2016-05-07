a198a6f3e9d6e21f243f507dd3a2a2e23c60cc2a
Nikita <nks.off@gmail.com>
2016-05-07 23:12:36 +0300

    init game models classes

---
+++ assets/js/game/game-session.js
@@ -0,0 +1,9 @@
+define(function(require) {
+  var Backbone = require('Backbone');
+  var _ = require('underscore');
+
+  var GameSession = Backbone.Model.extend({
+  });
+
+  return GameSession;
+});
---
+++ assets/js/game/game-user.js
@@ -0,0 +1,9 @@
+define(function(require) {
+  var Backbone = require('Backbone');
+  var _ = require('underscore');
+
+  var GameUser = Backbone.Model.extend({
+  });
+
+  return GameUser;
+});
